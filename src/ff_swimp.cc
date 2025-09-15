67051b782ea2e1f410c28637287a362380826b32
Pingan Xu <pingan.xu@outlook.com>
2025-09-15 14:10:05 +0000

    add fp32 to fp4 condition 2

--- src/ff_swimp.cc
+++ src/ff_swimp.cc
@@ -11,12 +11,24 @@ fp4_t cvt_fp32_to_fp4(fp32_t fp32_value) {
         res.value = 0x7;
         return res;
     }
+    uint32_t absx = fp32_value.value & 0x7fffffffU;
+    if (absx <= 0x3e800000U) {
+        // <= 0.25, res = {s, 0b000}
+        res.field.sign = fp32_value.field.sign;
+        return res;
+    }
+    else if (absx > 0x3e800000U && absx < 0x3f400000U) {
+        // > 0.25 && < 0.75, res = {s, 0b001}
+        res.field.sign = fp32_value.field.sign;
+        res.field.man = 0x1U;
+        return res;
+    }
+
     /*
         0.5
-        fp32(0.5) =  s 01111110 00000000000000000000000
+        fp32(0.5) =  s 01111110 10000000000000000000000
         min =        s 01111101 11000000000000000000000  >= 1.75 * 2 ^ -2
     */
-    
 }
 
 } // namespace swimp
