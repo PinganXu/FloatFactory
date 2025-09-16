7010e0f32c29a2f30c87677e7efa010fd1b70617
Pingan Xu <pingan.xu@outlook.com>
2025-09-16 13:27:31 +0000

    finish sw imp fp32 to fp4 coversion

--- src/ff_swimp.cc
+++ src/ff_swimp.cc
@@ -6,29 +6,47 @@ namespace swimp {
 fp4_t cvt_fp32_to_fp4(fp32_t fp32_value) {
     fp4_t res;
     res.value = 0;
-    if (fp32_value.field.exp == 0xff && fp32_value.field.man != 0) {
-        // NAN converts to positive FP4_MAXNORM
-        res.value = 0x7;
-        return res;
-    }
     uint32_t absx = fp32_value.value & 0x7fffffffU;
     if (absx <= 0x3e800000U) {
         // <= 0.25, res = {s, 0b000}
         res.field.sign = fp32_value.field.sign;
-        return res;
     }
     else if (absx > 0x3e800000U && absx < 0x3f400000U) {
         // > 0.25 && < 0.75, res = {s, 0b001}
         res.field.sign = fp32_value.field.sign;
         res.field.man = 0x1U;
-        return res;
     }
-
-    /*
-        0.5
-        fp32(0.5) =  s 01111110 10000000000000000000000
-        min =        s 01111101 11000000000000000000000  >= 1.75 * 2 ^ -2
-    */
+    else if (absx >= 0x3f400000U && absx <= 0x3fa00000U) {
+        // >= 0.75 && <= 1.25, res = {s, 0b010}
+        res.field.sign = fp32_value.field.sign;
+        res.field.exp = 0x1U;
+    }
+    else if (absx > 0x3fa00000U && absx < 0x3fe00000U) {
+        // > 1.25 && < 1.75, res = {s, 0b011}
+        res.value = 0x3U;
+        res.field.sign = fp32_value.field.sign;
+    }
+    else if (absx >= 0x3fe00000U && absx <= 0x40200000U) {
+        // >= 1.75 && <= 2.5, res = {s, 0b100}
+        res.field.sign = fp32_value.field.sign;
+        res.field.exp = 0x2U;
+    }
+    else if (absx > 0x40200000U && absx < 0x40600000U) {
+        // > 2.5 && < 3.5, res = {s, 0b101}
+        res.value = 0x5U;
+        res.field.sign = fp32_value.field.sign;
+    }
+    else if (absx >= 0x40600000U && absx <= 0x40a00000U) {
+        // >= 3.5 && <= 5, res = {s, 0b110}
+        res.field.exp = 0x3U;
+        res.field.sign = fp32_value.field.sign;
+    }
+    else { // absx > 0x40a00000U
+        res.value = 0x7U;
+        // NAN converts to {0b0111}
+        res.field.sign = (fp32_value.field.exp == 0xff && fp32_value.field.man != 0) ? 0x0U : fp32_value.field.sign;
+    }
+    return res;
 }
 
 } // namespace swimp
